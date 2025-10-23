45ca4ff24cced1a39501c01eee3f58144535a0d5
Kompanjuu <96250497+Kompanjuu@users.noreply.github.com>
2025-10-23 12:49:17 +0000

    Added Randomness

--- hello.py
+++ hello.py
@@ -1,18 +1,23 @@
-q1 = "Vilken är Sveriges Huvudstad?"
-q1_alt = ["Oslo","Stockholm","Köpenhamn"]
-q1_right = "Stockholm"
+import random
 
+questions = ["Vilken är Sveriges Huvudstad?","Vad heter senaste Minecraft versionen?"]
+alternatives = [["Oslo","Stockholm","Köpenhamn"],["1.22.1","1.21.10","1.21.4","1.22.5","1.22","1.21.8"]]
+rights = ["Stockholm","1.21.10"]
 
-def question(question,alternatives,correct):
+def question(question,alts,correct):
     print(question)
-    for i in range(len(alternatives)):
-        print(f"{i+1}. {alternatives[i]}")
+    random.shuffle(alts)
+    for i in range(len(alts)):
+        print(f"{i+1}. {alts[i]}")
     answer = int(input("Vilket Nummer? "))
-    if alternatives[answer-1] == correct:
+    if alts[answer-1] == correct:
+        print("Rätt")
         return 1
     else:
+        print(f"Fel, rätt svar var {correct}")
         return 0
 
 score = 0
-score += question(q1,q1_alt,q1_right)
-print(f"Your score is {score}\n")
+for i in range(len(questions)):
+    score += question(questions[i],alternatives[i],rights[i])
+    print(f"Your score is {score} / {i+1}\n")
